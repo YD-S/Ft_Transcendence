1a813aeedf82e36da54a4403f7d3a764b57a4769
kolterdyx <info@cirogarcia.dev>
2024-05-26 03:31:01 +0200

    fix game sockets

--- Game/consumers.py
+++ Game/consumers.py
@@ -6,6 +6,7 @@ from channels.generic.websocket import WebsocketConsumer
 
 class GameConsumer(WebsocketConsumer):
     players = 0
+    player_names = []
 
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
@@ -14,11 +15,12 @@ class GameConsumer(WebsocketConsumer):
     def connect(self):
         if self.players < 2:
             self.accept()
+            self.game_id = self.scope["url_route"]["kwargs"]["game_id"]
+            self.create_group(self.game_id + self.scope['user'].username)
+            self.player_names.append(self.scope['user'].username)
+            print(self.game_id)
         else:
-            pass
-        if self.players == 2:
-            self.create_group(self.scope["url_route"]["kwargs"]["game_id"])
-        self.game_id = self.scope["url_route"]["kwargs"]["game_id"]
+            return
         self.players += 1
 
     def receive(self, text_data):
@@ -26,15 +28,15 @@ class GameConsumer(WebsocketConsumer):
         message = text_data_json['type']
         direction = text_data_json['direction']
         y = text_data_json['y']
-        playerId = text_data_json['playerId']
+        player_id = text_data_json['playerId']
         if message == 'move':
             if direction == 'left':
-                if playerId:
+                if player_id:
                     y = y + 0.1
                 else:
                     y = y - 0.1
                 async_to_sync(self.channel_layer.group_send)(
-                    self.scope['user'].username,
+                    self.game_id + self.player_names[0],
                     {
                         'type': 'move',
                         'data': {
@@ -43,12 +45,12 @@ class GameConsumer(WebsocketConsumer):
                     }
                 )
             elif direction == 'right':
-                if playerId:
+                if player_id:
                     y = y - 0.1
                 else:
                     y = y + 0.1
                 async_to_sync(self.channel_layer.group_send)(
-                    self.scope['user'].username,
+                    self.game_id + self.player_names[1],
                     {
                         'type': 'move',
                         'data': {
@@ -56,6 +58,7 @@ class GameConsumer(WebsocketConsumer):
                         }
                     }
                 )
+
     def create_group(self, group_name):
         async_to_sync(self.channel_layer.group_add)(
             group_name,
@@ -63,9 +66,9 @@ class GameConsumer(WebsocketConsumer):
         )
 
     def move(self, event):
+        print('event', event)
         data = event['data']
         self.send(text_data=json.dumps({
             'type': event['type'],
             'y': data['y'],
         }))
-
