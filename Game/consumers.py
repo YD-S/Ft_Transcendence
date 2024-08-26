5ed1a4e87bc7d489e8f763722dba42621e86e846
Ciro García Belmonte <info@cirogarcia.dev>
2024-08-26 04:23:44 +0200

    game stuff

--- Game/consumers.py
+++ Game/consumers.py
@@ -1,15 +1,93 @@
 import asyncio
 import json
 import math
+import random
+
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+GAME_SIZE = 1000
+COURT_RADIUS = 15
+PLAYER_WIDTH = GAME_SIZE / 350
+BALL_RADIUS = GAME_SIZE / 1500
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+    def __rmul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        return Vector(self.x / other, self.y / other)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def magnitude(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def normalize(self):
+        return self / self.magnitude()
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    def rotate(self, angle: float):
+        """
+        Rotate the vector by a given angle in radians
+        :param angle: The angle in radians
+        """
+        return Vector(self.x * math.cos(angle) - self.y * math.sin(angle),
+                      self.x * math.sin(angle) + self.y * math.cos(angle))
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def project(self, other):
+        return other * (self.dot(other) / other.magnitude() ** 2)
+
+    def reflect(self, normal):
+        return self - normal * 2 * self.dot(normal)
+
+    def bounce(self, normal):
+        return self.reflect(normal).normalize()
+
+    def to_tuple(self):
+        return self.x, self.y
+
+    def to_list(self):
+        return [self.x, self.y]
+
+    @staticmethod
+    def from_angle(angle):
+        return Vector(math.cos(angle), math.sin(angle))
 
 
 class GameConsumer(AsyncWebsocketConsumer):
     players = 0
     player_names = []
-    player1_y = math.pi / 2
-    player2_y = -math.pi / 2
+    player1_angle = math.pi / 2
+    player2_angle = -math.pi / 2
     last_collision = None
+    ball_pos = Vector(0, 0)
+    ball_velocity = Vector.from_angle(random.random() * 2 * math.pi).normalize()
+    ball_speed = 0.1
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -39,14 +117,30 @@ class GameConsumer(AsyncWebsocketConsumer):
         if message == 'move':
             direction = text_data_json['direction']
             await self.calculate_y(direction, amIfirst)
-        elif message == 'ball_collision':
-            await self.calculate_ball_collision(text_data_json)
         elif message == 'initial_data':
             await self.setPlayer(amIfirst, text_data_json)
 
-    async def calculate_ball_collision(self, text_data_json):
-        paddleColided = text_data_json['PlayerId']
-        GameConsumer.last_collision = self.player1 if paddleColided == self.player1 else self.player2
+    async def calculate_ball_collision(self):
+        # difference between vector angles < angle span from center to paddle edges
+        ball_distance = self.ball_pos.magnitude()
+
+        margin = Vector(BALL_RADIUS + PLAYER_WIDTH / 2, COURT_RADIUS).angle()
+        diff1 = abs(self.ball_pos.angle() - GameConsumer.player1_angle)
+        diff2 = abs(self.ball_pos.angle() - GameConsumer.player2_angle)
+
+        print(f'diff1: {diff1} {margin} {diff1 < margin}')
+        print(f'diff1: {diff2} {margin} {diff2 < margin}')
+        if ball_distance > COURT_RADIUS + 1:
+            print("OUT OF BOUNDS")
+            if diff1 < margin:
+                GameConsumer.last_collision = self.player1
+                print("COLLISION PLAYER 1")
+            elif diff2 < margin:
+                GameConsumer.last_collision = self.player2
+                print("COLLISION PLAYER 2")
+            self.reset_ball()
+        else:
+            GameConsumer.last_collision = None
 
     async def setPlayer(self, amIfirst, text_data_json):
         if amIfirst:
@@ -55,14 +149,14 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.player2 = text_data_json['Player2']
 
     async def calculate_y(self, direction, amIfirst):
-        y = GameConsumer.player1_y if amIfirst else GameConsumer.player2_y
+        y = GameConsumer.player1_angle if amIfirst else GameConsumer.player2_angle
         increment = 0.01 if 'left' in direction else -0.01
         y += increment
         y = y % (2 * math.pi)
         if amIfirst:
-            GameConsumer.player1_y = y
+            GameConsumer.player1_angle = y
         else:
-            GameConsumer.player2_y = y
+            GameConsumer.player2_angle = y
 
     async def create_group(self, group_name):
         await self.channel_layer.group_add(
@@ -76,6 +170,8 @@ class GameConsumer(AsyncWebsocketConsumer):
             'type': event['type'],
             'player1_y': data['player1_y'],
             'player2_y': data['player2_y'],
+            'ball_x': data['ball_x'],
+            'ball_y': data['ball_y'],
         }))
 
     async def disconnect(self, close_code):
@@ -96,20 +192,36 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def send_every_frame(self, frame_rate=60):
         frame_duration = 1 / frame_rate
-        GameConsumer.player1_y = math.pi / 2
-        GameConsumer.player2_y = -math.pi / 2
+        GameConsumer.player1_angle = math.pi / 2
+        GameConsumer.player2_angle = -math.pi / 2
+        self.reset_ball()
         try:
             while True:
+                await self.ball_movement()
                 await self.channel_layer.group_send(
                     self.game_id,
                     {
                         'type': 'move',
                         'data': {
-                            'player1_y': GameConsumer.player1_y,
-                            'player2_y': GameConsumer.player2_y,
+                            'player1_y': GameConsumer.player1_angle,
+                            'player2_y': GameConsumer.player2_angle,
+                            'ball_x': GameConsumer.ball_pos.x,
+                            'ball_y': GameConsumer.ball_pos.y,
                         }
                     }
                 )
                 await asyncio.sleep(frame_duration)
         except asyncio.CancelledError:
             pass
+
+    async def ball_movement(self):
+        if GameConsumer.last_collision is not None:
+            self.reset_ball()
+        else:
+            GameConsumer.ball_pos += GameConsumer.ball_velocity * GameConsumer.ball_speed
+            await self.calculate_ball_collision()
+
+    def reset_ball(self):
+        GameConsumer.last_collision = None
+        GameConsumer.ball_pos = Vector(0, 0)
+        GameConsumer.ball_velocity = Vector.from_angle(random.random() * 2 * math.pi).normalize()
