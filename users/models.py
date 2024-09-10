4b9b3208f85e21a92aedecfa9ee273a7ce073d54
kolterdyx <info@cirogarcia.dev>
2024-09-10 19:20:03 +0200

    move media

--- users/models.py
+++ users/models.py
@@ -57,7 +57,7 @@ class User(AbstractUser, BaseModel):
     is_oauth = models.BooleanField(default=False)
     verified_email = models.BooleanField(default=False)
 
-    avatar = models.ImageField(upload_to="avatars/", null=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, default='svg/profile_icon.svg')
 
     objects = UserManager()
 
