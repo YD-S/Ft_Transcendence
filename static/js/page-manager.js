6694e364388d0780af4ccc228321039a06e61867
Eduardo Zafra <duduzafra@gmail.com>
2024-10-02 22:30:29 +0200

    cube loader

--- static/js/page-manager.js
+++ static/js/page-manager.js
@@ -1,100 +1,105 @@
 export class PageManager {
+	previousPage = null;
 
-    previousPage = null;
+	onLoadCallbacks = {};
+	onUnloadCallbacks = {};
 
-    onLoadCallbacks = {};
-    onUnloadCallbacks = {};
+	static __instance = null;
 
-    static __instance = null;
+	constructor() {
+		this.contentRoot = document.getElementById("root");
+		window.addEventListener("popstate", (event) => {
+			this.contentRoot.innerHTML = event.state.data;
+		});
 
+		if (PageManager.__instance === null) {
+			PageManager.__instance = this;
+		}
 
-    constructor() {
-        this.contentRoot = document.getElementById('root');
-        window.addEventListener('popstate', (event) => {
-            this.contentRoot.innerHTML = event.state.data;
-        });
+		return PageManager.__instance;
+	}
 
-        if (PageManager.__instance === null) {
-            PageManager.__instance = this;
-        }
+	/**
+	 * Get the singleton instance of the PageManager
+	 * @returns {PageManager} The singleton instance of the PageManager
+	 */
+	static getInstance() {
+		if (this.__instance === null) {
+			this.__instance = new PageManager();
+		}
+		return this.__instance;
+	}
 
-        return PageManager.__instance
-    }
+	/**
+	 * Load a page into the content root
+	 * @param page {string} The page to load
+	 * @param preserve_query {boolean} Whether to preserve the query string in the URL
+	 * @param options {object} Options for loading the page
+	 */
+	load(page, preserve_query = true, options = {}) {
+		this.loadToContainer(page, this.contentRoot, preserve_query, options);
+	}
 
-    /**
-     * Get the singleton instance of the PageManager
-     * @returns {PageManager} The singleton instance of the PageManager
-     */
-    static getInstance() {
-        if (this.__instance === null) {
-            this.__instance = new PageManager();
-        }
-        return this.__instance;
-    }
+	loadToContainer(page, container, preserve_query = true, options = {}) {
+		this.setLoading(container);
+		fetch(`/page/${page}.html` + (options.query || ""))
+			.then((response) => {
+				if (response.status !== 200) {
+					if (response.status === 401) {
+						this.load("auth/login");
+					} else {
+						this.load("home");
+					}
+					return "__redirected__";
+				}
+				return response.text();
+			})
+			.then((data) => {
+				if (data === "__redirected__") {
+					return;
+				}
+				if (options.storeInHistory !== false) {
+					history.pushState({ data: data }, "", "/" + page + (preserve_query ? window.location.search : ""));
+				}
+				container.innerHTML = data;
+				if (this.onUnloadCallbacks[this.previousPage]) {
+					this.onUnloadCallbacks[this.previousPage]();
+				}
+				if (this.onLoadCallbacks[page]) {
+					this.onLoadCallbacks[page](...(options.args || []));
+				}
+				if (options.storeInHistory !== false) {
+					this.previousPage = page;
+				}
+			});
+	}
 
-    /**
-     * Load a page into the content root
-     * @param page {string} The page to load
-     * @param preserve_query {boolean} Whether to preserve the query string in the URL
-     * @param options {object} Options for loading the page
-     */
-    load(page, preserve_query = true, options = {}) {
-        this.loadToContainer(page, this.contentRoot, preserve_query, options)
-    }
+	/**
+	 * Set a callback to be called when a page is loaded
+	 * @param page {string} The page to set the callback for
+	 * @param callback {function} The callback to be called when the page is loaded
+	 */
+	setOnPageLoad(page, callback) {
+		this.onLoadCallbacks[page] = callback;
+	}
 
-    loadToContainer(page, container, preserve_query = true, options = {}) {
-        this.setLoading(container);
-        fetch(`/page/${page}.html` + (options.query || ""))
-            .then(response => {
-                if (response.status !== 200) {
-                    if (response.status === 401) {
-                        this.load('auth/login')
-                    } else {
-                        this.load('home')
-                    }
-                    return '__redirected__'
-                }
-                return response.text();
-            })
-            .then(data => {
-                if (data === '__redirected__') {
-                    return
-                }
-                if (options.storeInHistory !== false) {
-                    history.pushState({data: data}, "", '/' + page + (preserve_query ? window.location.search : ""));
-                }
-                container.innerHTML = data;
-                if (this.onUnloadCallbacks[this.previousPage]) {
-                    this.onUnloadCallbacks[this.previousPage]();
-                }
-                if (this.onLoadCallbacks[page]) {
-                    this.onLoadCallbacks[page](...(options.args || []));
-                }
-                if (options.storeInHistory !== false) {
-                    this.previousPage = page;
-                }
-            });
-    }
+	/**
+	 * Set a callback to be called when a page is unloaded
+	 * @param page {string} The page to set the callback for
+	 * @param callback {function} The callback to be called when the page is unloaded
+	 */
+	setOnPageUnload(page, callback) {
+		this.onUnloadCallbacks[page] = callback;
+	}
 
-    /**
-     * Set a callback to be called when a page is loaded
-     * @param page {string} The page to set the callback for
-     * @param callback {function} The callback to be called when the page is loaded
-     */
-    setOnPageLoad(page, callback) {
-        this.onLoadCallbacks[page] = callback;
-    }
-
-    /**
-     * Set a callback to be called when a page is unloaded
-     * @param page {string} The page to set the callback for
-     * @param callback {function} The callback to be called when the page is unloaded
-     */
-    setOnPageUnload(page, callback) {
-        this.onUnloadCallbacks[page] = callback;
-    }
-
-    setLoading(container) {
-        container.innerHTML = "<div class='loader'>Loading</div>";
-    }
+	setLoading(container) {
+		container.innerHTML = `<div class='loader'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>`;
+	}
 }
