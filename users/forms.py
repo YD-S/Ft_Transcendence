f11e4a9f48020d89c1dca4c49e082e9a7ba5e4b2
Ciro García Belmonte <info@cirogarcia.dev>
2024-10-29 15:51:39 +0100

    fixes

--- users/forms.py
+++ users/forms.py
@@ -5,9 +5,22 @@ from users.models import User
 
 class UserForm(forms.ModelForm):
     avatar = forms.ImageField(required=False)
+    email = forms.EmailField(required=False)
+    password = forms.CharField(required=False)
 
     class Meta:
         model = User
         fields = [
-            'avatar'
+            'avatar',
+            'email',
+            'password',
         ]
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        if "email" in self.changed_data:
+            user.verified_email = False
+            user.has_2fa = False
+        if "password" in self.changed_data:
+            user.set_password(self.cleaned_data["password"])
+        super().save(*args, **kwargs)
